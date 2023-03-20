02eea734d2f738b967318b1604ecfdd2e50094ec
jooooonj <lee22424205@naver.com>
2023-03-20 21:43:35 +0900

    [BOJ] 카드2 / Silver4

--- src/백준/Baekjoon2164/Main.java
+++ src/백준/Baekjoon2164/Main.java
@@ -3,28 +3,26 @@ package 백준.Baekjoon2164;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            q.add(i + 1);
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N-1; j++){
-                if(arr[j]>arr[j+1]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
-                }
-            }
+        while(q.size()>1){
+            q.poll();
+            q.add(q.poll());
         }
 
-        for(int num : arr)
-            System.out.println(num);
+        int result = q.poll();
+
+        System.out.println(result);
     }
 }
